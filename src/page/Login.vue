0177498c62ac8a27942fabb1beb1b9132f00502c
futo-nakagawa <2210245@ecc.ac.jp>
2025-06-11 16:26:36 +0900

    first commit

---
+++ src/page/Login.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="login">
+        <h2>ログイン</h2>
+        <input v-model="email" type="text" name="email" placeholder="example@email.com">
+        <input v-model="password" type="text" name="password" placeholder="password">
+        <button @click="login">ログイン</button>
+        <button @click="signup">サインアップ</button>
+    </div>
+</template>
