95db47013c8bc068a442f7babfc967937eaf0759
futo-nakagawa <2210245@ecc.ac.jp>
2025-06-19 13:09:24 +0900

    fix firebase関連移動＆分割

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,4 +1,4 @@
-import {auth, db} from '../firebase'
+import {auth, db} from './firebase'
 import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth'
 import {doc, setDoc} from 'firebase/firestore'
 
@@ -6,14 +6,22 @@ export const loginUser = async (email: string, password: string) => {
     return signInWithEmailAndPassword(auth, email, password)
 }
 
-export const signupUser = async (email: string, password: string) => {
+export const signupUser = async (email: string, password: string, name: string) => {
     const result = await createUserWithEmailAndPassword(auth, email, password)
 
     const userRef = doc(db, 'users', result.user.uid)
     await setDoc(userRef,{
         uid: result.user.uid,
+        name: name,
         email: result.user.email,
         createdAt: new Date()
     })
     return result
 }
+
+export const logout = async () => {
+  await auth.signOut()
+};
+
+
+
